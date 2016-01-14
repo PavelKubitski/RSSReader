1ee5fbe962a386dea53484acecc25fbb9020868d
PavelKubitski <pavelkubitski@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-01-14 14:04:34 +0300

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Pavel Kubitski on 14.01.16.
+//  Copyright (c) 2016 Pavel Kubitski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by Pavel Kubitski on 14.01.16.
+//  Copyright (c) 2016 Pavel Kubitski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
